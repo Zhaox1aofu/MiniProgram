a8fe1542519c4a5a2b88e2e57625a7ed51b74db6
Zhaox1aofu <841432551@qq.com>
2021-09-13 16:07:03 +0800

    注册小程序和页面

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,35 @@
 // pages/home/home.js
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-
+        name: 'Zhao',
+        age: 18,
+        count: 0,
+        list: []
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
+    onLoad () {
+        wx.request({
+            url: 'http://localhost:3000/recommend',
+            success: (res) => {
+                console.log(res)
+                const data = res.data.data.list
+                this.setData({
+                    list: data
+                })
+            }
+        })
     },
+    handleBtnClick () {
+        // 1.错误做法
+        // this.data.count += 1
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
+        // 2.this.setData()
+        this.setData({
+            count: this.data.count + 1
+        })
     },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    handleSubBtnClick () {
+        this.setData({
+            count: this.data.count - 1
+        })
     }
 })
