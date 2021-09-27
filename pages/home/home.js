bb2db9d47f6ab547f61883dbccea46df88254771
Zhaox1aofu <841432551@qq.com>
2021-09-27 15:40:38 +0800

    网络请求及封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,50 @@
 // pages/home/home.js
+import request from '../../service/network.js'
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
 
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
     onLoad: function (options) {
+        //发送网络请求 原生方式
+        this.get_data_origin()
 
+        //2.发送网络请求 使用封装的request发送网络请求
+        // Promise最大的好处就是防止出现会掉地狱
+        request({
+            url: 'http://localhost:3000/recommend',
+
+        }).then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    get_data_origin() {
+        // wx.request({
+        //   url: 'http://localhost:3000/home/data',
+        //   data: {
+        //     type: 'sell',
+        //     page: 1
+        //   },
+        //   success: function(res) {
+        //       console.log(res)
+        //   }
+        // })
+        // post请求，并且携带参数
+        // wx.request({
+        //     url: 'http://httpbin.org/post',
+        //     method: 'POST',
+        //     data: {
+        //       name: 'Z',
+        //       age: 18
+        //     },
+        //     success: function(res) {
+        //         console.log(res)
+        //     },
+        //     fail: function(err) {
+        //         console.log(err)
+        //     }
+        //   })
     }
 })
